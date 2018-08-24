a03828a02f96082c1efe2c2103b6d8cadba4ea4d
Tyler Wasson <wasson@Tylers-MBP.hsd1.pa.comcast.net>
2018-08-24 11:46:34 -0400

    Laid Boilerplate for app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+    componentWillMount() {
+          // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyBp2y6KsMrgDjzpZjsbQB__EMtqxMdjkJo',
+            authDomain: 'manager-781d8.firebaseapp.com',
+            databaseURL: 'https://manager-781d8.firebaseio.com',
+            projectId: 'manager-781d8',
+            storageBucket: 'manager-781d8.appspot.com',
+            messagingSenderId: '963891894911'
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>Hello</Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
