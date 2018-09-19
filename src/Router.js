b35cd370e93a6e9d61d7f609394c16b3f665fb66
Tyler Wasson <wasson@Tylers-MacBook-Pro.local>
2018-09-19 15:59:40 -0400

    Added createEmployee, and firebase save as well as navigation

---
+++ src/Router.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Scene, Router, Stack, Actions } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
+const RouterComponent = () => {
+    return (
+        <Router>
+            <Stack key="root" hideNavBar>
+                <Stack key="auth">
+                    <Scene
+                        key="login"
+                        component={LoginForm}
+                        title="Login"
+                        initial
+                    />
+                </Stack>
+                <Stack key="main">
+                    <Scene
+                        key="employeeList"
+                        component={EmployeeList}
+                        title="Employees"
+                        rightTitle="Add"
+                        onRight={() => { Actions.employeeCreate(); }}
+                        initial
+                    />
+                    <Scene
+                        key="employeeCreate"
+                        component={EmployeeCreate}
+                        title="Create Employee"
+                    />
+                </Stack>
+            </Stack>
+        </Router>
+    );
+};
+
+export default RouterComponent;
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>test</Text>
+                <Text>test</Text>
+                <Text>test</Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeList;
